fa8234b0c26163d8e1174caefa56716191d76dc2
PeterEisenhauer94 <Petereisenhauer94@gmail.com>
2021-08-09 11:11:26 -0400

    refactored module name

--- src/test/java/com/example/ChurningTest.java
+++ src/test/java/com/example/ChurningTest.java
@@ -3,11 +3,11 @@ package com.example;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class ChurningTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(ChurningPlugin.class);
 		RuneLite.main(args);
 	}
 }
