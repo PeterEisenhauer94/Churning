4f2b57eb40c53a3420923b7fe932f9d1288a2a07
PeterEisenhauer94 <Petereisenhauer94@gmail.com>
2021-08-15 14:47:59 -0400

    updated  churningconfig

--- src/main/java/com/churning/ChurningConfig.java
+++ src/main/java/com/churning/ChurningConfig.java
@@ -4,16 +4,17 @@ import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
-@ConfigGroup("example")
+@ConfigGroup("Churning")
 public interface ChurningConfig extends Config
 {
 	@ConfigItem(
-		keyName = "greeting",
-		name = "Welcome Greeting",
-		description = "The message to show to the user when they login"
+		keyName = "DisplayOverlay",
+		name = "Churning Overlay",
+		description = "Determines whether to show the churning overlay or not",
+		position = 1
 	)
-	default String greeting()
+	default boolean DisplayOverlay()
 	{
-		return "Hello";
+		return true;
 	}
 }
