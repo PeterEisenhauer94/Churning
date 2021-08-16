0d2b638248cd04db97ab4df754060943543e226e
PeterEisenhauer94 <Petereisenhauer94@gmail.com>
2021-08-16 07:07:54 -0400

    started setting up overlay

--- src/main/java/com/churning/ChurningConfig.java
+++ src/main/java/com/churning/ChurningConfig.java
@@ -15,6 +15,6 @@ public interface ChurningConfig extends Config
 	)
 	default boolean DisplayOverlay()
 	{
-		return false;
+		return true;
 	}
 }
--- src/main/java/com/churning/ChurningOverlay.java
+++ src/main/java/com/churning/ChurningOverlay.java
@@ -1,4 +1,33 @@
 package com.churning;
 
-public class ChurningOverlay {
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.util.Set;
+import javax.inject.Inject;
+import net.runelite.api.Client;
+import static net.runelite.api.MenuAction.RUNELITE_OVERLAY_CONFIG;
+import net.runelite.client.ui.overlay.OverlayMenuEntry;
+import net.runelite.client.ui.overlay.OverlayPanel;
+import net.runelite.client.ui.overlay.OverlayPosition;
+import net.runelite.client.ui.overlay.OverlayPriority;
+import net.runelite.client.ui.overlay.components.LineComponent;
+import net.runelite.client.util.ColorUtil;
+
+public class ChurningOverlay extends OverlayPanel
+{
+    private final Client client;
+    private final ChurningConfig config;
+    private final ChurningPlugin plugin;
+
+    @Inject
+    private ChurningOverlay(Client client, ChurningConfig config, ChurningPlugin plugin)
+
+
+
+
+
+
+
+
 }
